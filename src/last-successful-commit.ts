ef4a4dce775204d426a835d03d3bb3723dd137fe
Sebastian Comans <sebastian.comans@i40mc.de>
2023-08-25 12:18:36 +0200

    🐛 check commit hashes to prevent deleted hashes to be used as latest successful commit

--- src/last-successful-commit.ts
+++ src/last-successful-commit.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { exec } from '@actions/exec';
 
 export async function getLastSuccessfulCommit(
   token: string,
@@ -15,7 +16,19 @@ export async function getLastSuccessfulCommit(
     status: 'success',
     branch,
   });
-  const result = res.data.workflow_runs.length > 0 ? res.data.workflow_runs[0].head_commit.id : null;
-  core.info(`ℹ️ Last successful build: ${result}`);
+  let result = res.data.workflow_runs.length > 0 ? res.data.workflow_runs[0].head_commit.id : null;
+  if (result) {
+    const valid = await checkCommitHash(result);
+    if (!valid) {
+      result = null;
+    }
+  }
+
+  core.info(`ℹ️ Last successful build: ${ result ?? 'None' }`);
   return result;
+}
+
+async function checkCommitHash(hash: string): Promise<boolean> {
+  const result = await exec('git', ['cat-file', '-e', hash]);
+  return result === 0;
 }
